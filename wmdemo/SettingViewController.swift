090fff3312968db5d209c5b25409078d2d881d1b
Zhaosgithub <zhao.cmu@gmail.com>
2016-05-23 20:30:50 -0700

    add gif

---
+++ wmdemo/SettingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingViewController.swift
+//  wmdemo
+//
+//  Created by Zhaochang He on 5/23/16.
+//  Copyright © 2016 Zhaochang He. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = CGSize(width: 320, height: 1000)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ wmdemo/welcome to.swift
@@ -0,0 +1,16 @@
+//
+//  welcome to.swift
+//  wmdemo
+//
+//  Created by Zhaochang He on 5/17/16.
+//  Copyright © 2016 Zhaochang He. All rights reserved.
+//
+
+import UIKit
+
+class welcome_to: NSObject {
+
+
+    
+
+}
